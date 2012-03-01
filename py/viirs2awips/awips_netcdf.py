911958ea2498b77c6981e903e238db44720cc337
davidh <davidh@4d388797-7bbf-48c7-8be9-9a2fd9c7ae22>
2012-03-01 16:51:08 +0000

    first try add AWIPS NC creation created AWIPS grid 211 template created AWIPS NC to template script updated NC script to have a fill and create function for those 2 cases

--- py/viirs2awips/awips_netcdf.py
+++ py/viirs2awips/awips_netcdf.py
@@ -41,26 +41,36 @@ variables:
 """
 
 import os, sys, logging, re
+import shutil
 from numpy import *
 from netCDF4 import Dataset
+from keoni.fbf import Workspace
+
+log = logging.getLogger(__name__)
 
 AWIPS_ATTRS = set(re.findall(r'\W:(\w+)', NCDUMP))
 
 def create(nc_pathname, image, **attrs):
-    nc = Dataset(pathname, 'w', format='NETCDF3_CLASSIC')
+    # Clobber will not allow truncating of an existing file
+    nc = Dataset(pathname, 'w', format='NETCDF3_CLASSIC', clobber=False)
 
     r,c = image.shape
     dim_x = nc.createDimension('x', c)
     dim_y = nc.createDimension('y', r)
 
     if any((image > 255) | (image < 0)):
-        LOG.warning('image has values outside of unsigned byte range!')
-    if image.dtype != ubyte:
-        LOG.warning('converting image to uint8')
-        image = image.astype(ubyte)
+        log.warning('image has values outside of unsigned byte range!')
+    #if image.dtype != ubyte:
+    #    log.warning('converting image to uint8')
+    #    image = image.astype(ubyte)
+    if image.dtype != byte:
+        # netCDF4 should do this automatically, but just in case
+        log.warning('converting image to int8')
+        image = image.astype(byte)
 
-    var_image = nc.createVariable('image', 'u1', ('y', 'x'))
-    LOG.debug('image(%d,%d)' % (r,c))
+    var_image = nc.createVariable('image', 'b', ('y', 'x'))
+    #var_image = nc.createVariable('image', 'u1', ('y', 'x'))
+    log.debug('image(%d,%d)' % (r,c))
 
     # transfer attributes, converting floating point values to 32-bit
     want_attrs = set(AWIPS_ATTRS)
@@ -68,14 +78,90 @@ def create(nc_pathname, image, **attrs):
     for k,v in attrs.items():
         if issubdtype(type(v), float):
             v = float32(v)
-        LOG.debug('.%s = %r' % (k,v))
+        log.debug('.%s = %r' % (k,v))
         setattr(nc, k, v)
         have_attrs.add(k)
     if not want_attrs.issubset(have_attrs):
-        LOG.error('missing attributes: %r' % (want_attrs - have_attrs))
+        log.error('missing attributes: %r' % (want_attrs - have_attrs))
     nc.close()
 
+def fill(nc_name, image, template):
+    """Copy a template file to destination and fill it
+    with the provided image data.
+
+    WARNING: Timing information is not added
+    """
+    nc_name = os.path.abspath(nc_name)
+    template = os.path.abspath(template)
+    # TODO: How do I figure out the time?
+    if not os.path.exists(template):
+        log.error("Template does not exist %s" % template)
+        return False
+
+    if os.path.exists(nc_name):
+        log.error("Output file %s already exists" % nc_name)
+        return False
+
+    try:
+        shutil.copyfile(template, nc_name)
+    except StandardError:
+        log.error("Could not copy template file %s to destination %s" % (template,nc_name))
+        return False
+
+    nc = Dataset(nc_name, "a")
+    if nc.file_format != "NETCDF3_CLASSIC":
+        log.warning("Expected file format NETCDF3_CLASSIC got %s" % (nc.file_format))
+
+    image_var = nc.variables["image"]
+    if image_var.shape != image.shape:
+        log.error("Image shapes aren't equal")
+        return False
+
+    image_var[:] = image
+    # FIXME: Time needs to be derived from somewhere
+    time_var = nc.variables["validTime"]
+    time_var[:] = 1330537260
+    nc.sync() # Just in case
+    nc.close()
+    log.debug("Data transferred into NC file correctly")
+    return True
+
+def go(img_name, template, nc_name=None):
+    # Make up an NC name
+    img_name = os.path.abspath(img_name)
+    base_dir,img_fn = os.path.split(img_name)
+    var_name = img_fn.split(".")[0]
+    base_name = os.path.splitext(img_fn)[0]
+    if base_name == '':
+        log.error("Could not extract file's base name %s" % img_name)
+        return False
+
+    if nc_name is None:
+        nc_name = base_name + ".nc"
+        nc_path = os.path.join(base_dir, nc_name)
+    else:
+        nc_path = os.path.abspath(nc_name)
+
+    # Open the image file
+    try:
+        W = Workspace(base_dir)
+        img = getattr(W, var_name)[0]
+        discard = (img > 65530)
+        data = ma.masked_array(img, discard)
+    except StandardError:
+        log.error("Could not open img file %s" % img_name, exc_info=1)
+        return False
+
+    # Create the NC file
+    fill(nc_path, data, template)
+    return True
 
 if __name__=='__main__':
-    for fn in sys.argv[1:]:
-        go(fn)
+    logging.basicConfig(level=logging.WARNING)
+    if len(sys.argv) != 4 and len(sys.argv) != 3:
+        log.error("Need at least 2 arguments: <image> <template> [<output>]")
+        sys.exit(-1)
+
+    go(*sys.argv[1:])
+    #for fn in sys.argv[1:]:
+    #    go(fn)
---
+++ py/viirs2awips/nc_to_template.py
@@ -0,0 +1,78 @@
+"""Take a NetCDF file compatible with AWIPS or NCEP grids and replace
+all image data with an array of NaNs.  The output file should be used
+as a template: copied to a new location and then filled with actual
+image data.
+
+Author: David Hoese,davidh,SSEC
+"""
+import os
+import sys
+import shutil
+import logging
+from netCDF4 import Dataset
+from numpy import array,repeat,nan
+
+log = logging.getLogger(__name__)
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    usage = """
+Usage: python nc_to_template.py <input nc> <output nc>
+"""
+    args = sys.argv[1:]
+    if len(args) != 2:
+        log.error("Requires 2 arguments: input file and output file")
+        print usage
+        return -1
+
+    input_file = os.path.abspath(args[0])
+    output_file = os.path.abspath(args[1])
+
+    if not os.path.exists(input_file):
+        log.error("Input file %s does not exist" % input_file)
+        return -1
+
+    if os.path.exists(output_file):
+        log.error("Output file %s already exists" % output_file)
+        return -1
+
+    # Copy the original to the template location
+    try:
+        log.debug("Copying input file to output file location")
+        shutil.copyfile(input_file, output_file)
+    except StandardError:
+        log.error("Could not move input file to output location", exc_info=1)
+        return -1
+
+    # Open the template file
+    try:
+        log.debug("Opening output file to fill in data")
+        nc = Dataset(output_file, "a")#, format="NETCDF3_CLASSIC")
+    except StandardError:
+        log.error("Error trying to open NC file")
+        try:
+            log.debug("Trying to remove file that couldn't be opened %s" % output_file)
+            os.remove(output_file)
+        except StandardError:
+            log.warning("Bad output file could not be removed %s" % output_file)
+        finally:
+            return -1
+
+    if nc.file_format != "NETCDF3_CLASSIC":
+        log.warning("Expected file format NETCDF3_CLASSIC got %s" % nc.file_format)
+
+    # Change the data to all NaNs
+    if "image" not in nc.variables:
+        log.error("NC variable 'image' does not exist in %s" % input_file)
+        return -1
+
+    columns,rows = nc.variables["image"].shape
+    nc.variables["image"][:] = repeat(repeat([[nan]], columns, axis=1), rows, axis=0)
+    nc.variables["validTime"][:] = 0
+    nc.close()
+
+    log.info("Template created successfully")
+    return 0
+    
+if __name__ == "__main__":
+    sys.exit(main())
