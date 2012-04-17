d9303347c5d14c490fa11e35d57fd0d1262a9032
davidh <davidh@4d388797-7bbf-48c7-8be9-9a2fd9c7ae22>
2012-04-17 14:23:01 +0000

    made basic structure of the polar2grid package

---
+++ py/polar2grid/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = ""
+version = '0.0.4'
+
+setup(
+    name='polar2grid',
+    version=version,
+    description="Library and scripts to remap imager data to a grid",
+    classifiers=filter(None, classifiers.split("\n")),
+    keywords='',
+    author='David Hoese, SSEC',
+    author_email='david.hoese@ssec.wisc.edu',
+    url='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['numpy', 'netCDF4', 'matplotlib', 'h5py'],
+    dependency_links = ['http://larch.ssec.wisc.edu/cgi-bin/repos.cgi'],
+    entry_points = {'console_scripts' : [
+            'viirs2awips = polar2grid.viirs2awips:main',
+            ]}
+)
+
