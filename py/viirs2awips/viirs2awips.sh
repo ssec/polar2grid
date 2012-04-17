7e44ded702f32e01a0d740509e4271712b1b1a1a
davidh <davidh@4d388797-7bbf-48c7-8be9-9a2fd9c7ae22>
2012-04-17 17:59:39 +0000

    removed PROJECT_DIR from viirs2awips.sh, it wasn't needed

--- py/viirs2awips/viirs2awips.sh
+++ py/viirs2awips/viirs2awips.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 ### VIIRS2AWIPS WRAPPER ###
-# Assumes that it is called from the temporary directory where files should be placed
-
-PROJECT_DIR=$PWD
 
 if [ -z "$POLAR2GRID_HOME" ]; then 
   export POLAR2GRID_HOME="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
@@ -29,11 +26,8 @@ if [ -z "$VIIRS_SHAPE_CONFIG" ]; then
   export VIIRS_SHAPE_CONFIG=$PKG_BASE/awips_shapes.conf
 fi
 
-# Go back to the directory this was called from
-cd $PROJECT_DIR
-
 # Test configs/Egg installation
-#$POLAR2GRID_HOME/ShellB3/bin/python -m polar2grid.awips_config
+$POLAR2GRID_HOME/ShellB3/bin/python -m polar2grid.awips_config
 
 # Call the python module to do the processing, passing all arguments
-$POLAR2GRID_HOME/ShellB3/bin/python -m polar2grid.viirs2awips -vv $@
+#$POLAR2GRID_HOME/ShellB3/bin/python -m polar2grid.viirs2awips -vv $@
