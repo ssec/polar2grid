41816c305e97711a68e2a485f5a266222ce04774
rayg <rayg@4d388797-7bbf-48c7-8be9-9a2fd9c7ae22>
2012-04-24 14:44:25 +0000

    added --pass option which consumes the .nfo file and creates .pass directories with softlinks

--- py/util/dibs.py
+++ py/util/dibs.py
@@ -17,6 +17,7 @@ import os, sys, re
 from urllib2 import urlopen
 from subprocess import call
 from collections import defaultdict
+from glob import glob
 from datetime import date, timedelta, datetime
 
 LOG = logging.getLogger(__name__)
@@ -122,7 +123,7 @@ def sync(lat, lon, radius, start=None, end=None):
             new_files[key].append(nfo)
     return new_files
 
-def go(name, lat, lon, radius, start=None, end=None):
+def mainsync(name, lat, lon, radius, start=None, end=None):
     "write a .nfo file with 'date start_time end_time when we complete a transfer"
     lat = int(lat)
     lon = int(lon)
@@ -141,6 +142,99 @@ def go(name, lat, lon, radius, start=None, end=None):
 
 
 
+
+hmst = lambda s: tuple(map(int, [s[0:2], s[2:4], s[4:6], s[6]+'00000']))
+ymd = lambda s: tuple(map(int, [s[0:4], s[4:6], s[6:8]]))
+
+def _key2dts(k):
+    "convert (yyyymmdd, hhmmsst, hhmmsst) string key tuple into start and end datetime objects"
+    d,s,e = k
+    d = ymd(d)
+    s = hmst(s)
+    e = hmst(e)
+    ds = datetime(*(d+s))
+    de = datetime(*(d+e))
+    if de < ds:
+        de += timedelta(days=1)
+    return ds,de
+
+def _dts2key(s,e):
+    "convert datetime object into key tuple"
+    horus = lambda x: '%02d%02d%02d%d' % (x.hour, x.minute, x.second, x.microsecond/100000)
+    return s.strftime('%Y%m%d'), horus(s), horus(e)
+
+def _outcome_cg(seq):
+    "given a sequence of (start,end) ordered datetime objects representing contiguous granules, return new outer key and list of granules"
+    if not seq: return None
+    grans = [seq[0][0]] + [x[1] for x in seq]
+    # build new key
+    s = grans[0][0]
+    e = grans[-1][1]
+    return (_dts2key(s,e), [_dts2key(*x) for x in grans])
+
+def contiguous_groups(keyset, tolerance=timedelta(seconds=5)):
+    "sort a set of keys into contiguous groups; yield (newkey, list-of-subkeys) sequence"
+    granlist = list(sorted(map(_key2dts, keyset)))
+
+    # pair off in start-time order
+    # build a set containing contiguous granules
+    # when we find a break in sequence, yield the set as an ordered tuple and start over
+    seq = []
+    for ((sa,ea),(sb,eb)) in zip(granlist[:-1], granlist[1:]):
+        delta = sb-ea
+        if delta < tolerance:
+            seq.append(((sa,ea),(sb,eb)))
+        else:
+            if seq:
+                yield _outcome_cg(seq)
+            seq = []
+    if seq:
+        yield _outcome_cg(seq)
+
+def read_nfo(filename=None, fobj=None):
+    if fobj is None:
+        fobj = file(filename, 'rt')
+    for line in fobj:
+        k = map(str.strip, line.split(' '))
+        if len(k)==3:
+            yield k
+
+STAMP_FMT = 'd%s_t%s_e%s'
+
+def pass_build(key, subkeys):
+    "link all files belonging to subkeys to an pass directory"
+    name = STAMP_FMT % key
+    final_name = name + '.pass'
+    if os.path.isdir(name) or os.path.isdir(final_name):
+        LOG.warning('%s already has been processed' % name)
+        return None
+    os.mkdir(name)
+    for piece in subkeys:
+        pat = '*' + STAMP_FMT % piece + '*.h5'
+        LOG.debug('looking for %s' % pat)
+        for filename in glob(pat):
+            LOG.debug('linking %s to %s' % (filename, name))
+            os.symlink(os.path.join('..', filename), os.path.join(name, filename))
+    os.rename(name, final_name)
+    LOG.info('created %s' % final_name)
+
+
+
+def mainpass(nfo_filename):
+    "consume nfo file and link granule groups to their own .pass directories"
+    fobj = file(nfo_filename, 'rt')
+    stowname = '.' + nfo_filename
+    if os.path.isfile(stowname):
+        LOG.warning('removing old %s' % stowname)
+        os.unlink(stowname)
+    os.rename(nfo_filename, stowname)
+    nfo = list(read_nfo(fobj=fobj))
+    fobj.close()
+    passes = contiguous_groups(nfo)
+    for eve in passes:
+        pass_build(*eve)
+
+
 def main():
     import optparse
     usage = """
@@ -151,6 +245,7 @@ files which have already been downloaded will not be re-downloaded
 default start and end is yesterday~today
 example:
 %prog madison --lat=43 --lon=-89 --radius=1000
+%prog madison --pass
 
 """
     parser = optparse.OptionParser(usage)
@@ -160,9 +255,10 @@ example:
                     help='each occurrence increases verbosity 1 level through ERROR-WARNING-INFO-DEBUG')
     parser.add_option('-a', '--lat', dest='lat', help='central latitude', type='int')
     parser.add_option('-o', '--lon', dest='lon', help='central longitude', type='int')
-    parser.add_option('-r', '--radius', dest='radius', help='radius in km', type='int')
+    parser.add_option('-r', '--radius', dest='radius', help='radius in km', type='int', default=0)
     parser.add_option('-s', '--start', dest='start', help='yyyy-mm-dd start dateoptional', default=None)
     parser.add_option('-e', '--end', dest='end', help='yyyy-mm-dd end date optional', default=None)
+    parser.add_option('-p', '--pass', dest='passes', help='post-process .nfo file (consuming it) and create .pass directories', default=False, action="store_true")
     # parser.add_option('-o', '--output', dest='output',
     #                 help='location to store output')
     # parser.add_option('-I', '--include-path', dest="includes",
@@ -186,7 +282,13 @@ example:
         parser.error( 'incorrect arguments, try -h or --help.' )
         return 9
 
-    go(args[0], options.lat, options.lon, options.radius, options.start, options.end)
+    if options.radius:
+        mainsync(args[0], options.lat, options.lon, options.radius, options.start, options.end)
+
+    if options.passes:
+        mainpass(args[0]+'.nfo')
+        return 0
+
 
     return 0
 
