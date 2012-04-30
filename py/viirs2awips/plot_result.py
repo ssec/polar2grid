0f879ed8920475f8f57bb301cfac59720d9f2363
davidh <davidh@4d388797-7bbf-48c7-8be9-9a2fd9c7ae22>
2012-04-30 19:38:44 +0000

    forgot to remove plot_result.py from viirs2awips dir

--- py/viirs2awips/plot_result.py
+++
@@ -1,7 +0,0 @@
-from numpy import *
-from matplotlib import pyplot as plt
-from core import Workspace; W=Workspace('.')
-result=W.result
-plt.imshow(result)
-plt.bone()
-plt.savefig("plot_result.png")
