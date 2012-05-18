344bcbc57817b38cd3788898d938190ba6f74919
davidh <davidh@4d388797-7bbf-48c7-8be9-9a2fd9c7ae22>
2012-05-18 20:51:30 +0000

    made custom ncdiff in python

--- py/viirs2awips/v2a_ak_verify.sh
+++ py/viirs2awips/v2a_ak_verify.sh
@@ -38,6 +38,29 @@ for VFILE in $VERIFY_BASE/*; do
         oops "Could not find output file $WFILE"
     fi
     diff $VFILE $WFILE || oops "$WFILE was different than expected"
+    python <<EOF
+from netCDF4 import Dataset
+import numpy
+import sys
+
+nc1_name  = "$VFILE"
+nc2_name  = "$WFILE"
+threshold = 1
+
+nc1 = Dataset(nc1_name, "r")
+nc2 = Dataset(nc2_name, "r")
+image1_var = nc1.variables["image"]
+image2_var = nc2.variables["image"]
+image1_var.set_auto_maskandscale(False)
+image2_var.set_auto_maskandscale(False)
+if len(numpy.nonzero(numpy.abs(image2_var[:] - image1_var[:]) >= threshold)[0]) != 0:
+    sys.exit(1)
+else:
+    sys.exit(0)
+
+EOF
+[ $? -eq 0 ] || oops "$WFILE" was different than expected"
+
 done
 
 # End of all tests
