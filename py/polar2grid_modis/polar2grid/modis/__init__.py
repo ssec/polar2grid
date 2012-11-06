e51bf0707a21412d4f97d66158f4ecff9fbfb45f
Eva Schiffer <eva.schiffer@ssec.wisc.edu>
2012-11-06 17:54:27 +0000

    making some modis related things available outside the package

--- py/polar2grid_modis/polar2grid/modis/__init__.py
+++ py/polar2grid_modis/polar2grid/modis/__init__.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python
-from .bt import bright_shift
-
+from .bt              import bright_shift
+from .modis_to_swath  import make_swaths
+from .modis_guidebook import VIS_INF_FILE_PATTERN
