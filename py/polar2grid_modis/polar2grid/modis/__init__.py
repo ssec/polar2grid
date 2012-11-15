132812cc90c464d40e46485a9950e383d6b4a612
Eva Schiffer <eva.schiffer@ssec.wisc.edu>
2012-11-15 23:01:25 +0000

    allowing some bt conversion related things to be reached outside the modis module

--- py/polar2grid_modis/polar2grid/modis/__init__.py
+++ py/polar2grid_modis/polar2grid/modis/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 from .bt              import bright_shift
 from .modis_to_swath  import make_swaths
+from .modis_to_swath  import convert_radiance_to_bt
 from .modis_guidebook import VIS_INF_FILE_PATTERN
+from .modis_guidebook import SHOULD_CONVERT_TO_BT
