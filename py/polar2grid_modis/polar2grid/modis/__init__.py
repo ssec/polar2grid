e2f1d48ec7f01eb364977107628010b8387c4908
Eva Schiffer <eva.schiffer@ssec.wisc.edu>
2012-11-20 23:07:21 +0000

    added some methods and constants that are needed in the rest of the program

--- py/polar2grid_modis/polar2grid/modis/__init__.py
+++ py/polar2grid_modis/polar2grid/modis/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 from .bt              import bright_shift
 from .modis_to_swath  import make_swaths
-from .modis_to_swath  import convert_radiance_to_bt
-from .modis_guidebook import VIS_INF_FILE_PATTERN
+from .modis_filters   import convert_radiance_to_bt
+from .modis_filters   import make_data_cloud_cleared
+from .modis_filters   import create_fog_band
+from .modis_guidebook import GEO_FILE_GROUPING
 from .modis_guidebook import SHOULD_CONVERT_TO_BT
+from .modis_guidebook import IS_CLOUD_CLEARED
+from .modis_guidebook import CLOUDS_VALUES_TO_CLEAR
+from .modis_guidebook import BANDS_REQUIRED_TO_CALCULATE_FOG_BAND
+
