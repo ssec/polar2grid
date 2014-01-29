f7e29f7cf1ecec5e340b33281a333bb550f21892
davidh-ssec <david.hoese@ssec.wisc.edu>
2014-01-29 14:51:16 -0600

    Changed the threshold for crefl files where everything 16000 and above is now considered a fill value. Also allowed for Non-terrain corrected files with VIIRS CREFL.

--- py/polar2grid_crefl/polar2grid/crefl/crefl2swath.py
+++ py/polar2grid_crefl/polar2grid/crefl/crefl2swath.py
@@ -122,7 +122,8 @@ class Frontend(roles.FrontendRole):
         attrs = ds_object.attributes()
         if fill_attr_name:
             input_fill_value = attrs[fill_attr_name]
-            input_fill_value = 14000 # FIXME
+            log.debug("Using fill value attribute '%s' (%s) to filter bad data", fill_attr_name, str(input_fill_value))
+            input_fill_value = 16000
             fill_mask = band_data >= input_fill_value
         else:
             fill_mask = numpy.zeros_like(band_data).astype(numpy.bool)
