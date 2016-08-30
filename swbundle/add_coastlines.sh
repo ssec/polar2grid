9dafac4b33b917bd542b929ccd0e016769aeb565
davidh-ssec <david.hoese@ssec.wisc.edu>
2016-08-30 08:10:16 -0800

    Add bash wrappers for coastlines and colormaps scripts

---
+++ swbundle/add_coastlines.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# encoding: utf-8
+# Copyright (C) 2014 Space Science and Engineering Center (SSEC),
+#  University of Wisconsin-Madison.
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file is part of the polar2grid software package. Polar2grid takes
+# satellite observation data, remaps it, and writes it to a file format for
+# input into another program.
+# Documentation: http://www.ssec.wisc.edu/software/polar2grid/
+#
+#     Written by David Hoese    December 2014
+#     University of Wisconsin-Madison
+#     Space Science and Engineering Center
+#     1225 West Dayton Street
+#     Madison, WI  53706
+#     david.hoese@ssec.wisc.edu
+
+if [ -z "$POLAR2GRID_HOME" ]; then
+  export POLAR2GRID_HOME="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+fi
+
+# Setup necessary environments
+source $POLAR2GRID_HOME/bin/env.sh
+
+# Call the python module to do the processing, passing all arguments
+${P2G_SHELLB3_DIR}/bin/python -m polar2grid.add_coastlines $@
