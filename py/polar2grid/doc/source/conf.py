967109276b85e74bf17192c3ff8d534bbcf7ce4e
davidh-ssec <david.hoese@ssec.wisc.edu>
2016-11-30 21:48:34 -0600

    Fix sphinx math extension again

--- py/polar2grid/doc/source/conf.py
+++ py/polar2grid/doc/source/conf.py
@@ -42,7 +42,7 @@ rst_epilog = """
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.graphviz', 'sphinx.ext.todo', 'sphinx.ext.coverage',
-              # 'sphinx.ext.imgmath',
+              'sphinx.ext.imgmath',
               'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinxarg.ext']
 
 graphviz_dot = '/opt/local/bin/dot'
