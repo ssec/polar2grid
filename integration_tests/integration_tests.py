56a2d89703d3812cf7f132a69d9e86bf4cd99f88
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-03 18:55:07 +0000

    Added more logging

--- integration_tests/integration_tests.py
+++ integration_tests/integration_tests.py
@@ -1,6 +1,6 @@
+from argparse import ArgumentParser
 import os
 import subprocess
-from argparse import ArgumentParser
 import logging
 import filecmp
 import sys
@@ -9,37 +9,42 @@ import yaml
 import tempfile
 
 LOG = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+LOG.addHandler(handler)
+LOG.setLevel(logging.INFO)
+
 
 def run_test(p2g_path, test, base_dir):
     source = os.path.join(base_dir, test["source_dir"])
     command = test["command"]
     expected_dir = os.path.join(base_dir, test["expected_dir"]) 
 
-    p2g_path = os.path.join(p2g_path, command) + " "  + source
+    p2g_path = os.path.join(p2g_path, command) + " " + source
 
     orig_dir = os.getcwd()
     try: 
-        # FIXME delete later
-        if not os.path.isdir(expected_dir):
-            os.makedirs(expected_dir) 
-            os.chdir(expected_dir)
-            subprocess.call(p2g_path, shell=True)
-            os.chdir(orig_dir)
-        
+        #if not os.path.isdir(expected_dir):
+        #    os.makedirs(expected_dir) 
+        #    os.chdir(expected_dir)
+        #    subprocess.call(p2g_path, shell=True)
+        #    os.chdir(orig_dir)
+        LOG.info("Making temporary directory...")         
         temp_dir = tempfile.mkdtemp()
         os.chdir(temp_dir)
         subprocess.call(p2g_path, shell=True)
         
+        # currently in the temp dir
         for dirpaths, dirs, files in os.walk(expected_dir):
             for f in files: 
-                if os.path.isfile(f):
+                if os.path.isfile(f): # if the file was created in the temp dir  
                     if filecmp.cmp(f, os.path.join(dirpaths, f)):
-                        LOG.info("Files match")
-                else:
-                    raise Exception("files do not match")
+                        LOG.info("{} matches expected output".format(f))
+                else: 
+                    LOG.warning("{} was not created".format(f))
     finally:
         os.chdir(orig_dir)
     
+    LOG.info("Removing temporary directory...")
     shutil.rmtree(temp_dir)    
 
 
@@ -54,7 +59,8 @@ def main():
     
     stream = file(yfilename, "r")
     yfile = yaml.load(stream)
-
+    
+    LOG.info("Running tests...")
     for test in yfile["tests"]:
         run_test(p2g_path, test, yfile["base_test_dir"])
   
---
+++ integration_tests/run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python /data/users/kkolman/integration_tests/polar2grid/integration_tests/integration_tests.py /data/dist/polar2grid-swbundle-2.2.1b0/bin/ /data/users/kkolman/integration_tests/polar2grid/integration_tests/config.yaml
