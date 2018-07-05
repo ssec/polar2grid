d5821eb0a2e4ca7e4cb7830c851371063548b821
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-05 19:47:48 +0000

    Added exit after feature failure

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -3,5 +3,13 @@
 #def before_all(context):
 #    logging.basicConfig(filemode='w', filename="/data/users/kkolman/integration_tests/polar2grid/integration_tests/behave.log", level=logging.INFO, format='%(levelname)s: %(message)s')
 #    context.logger = logging.getLogger(__name__)
+
+import sys
     
+def after_feature(context, feature):
+    if context.failed:
+        sys.exit(1)
+
+
+
 
