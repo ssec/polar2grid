6e2677437ce63e120ffeda91aa459fcd8deff60c
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-06 18:35:08 +0000

    Testing diff run.sh exit

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#python integration_tests.py /data/dist/polar2grid-swbundle-2.2.1b0/bin/ config.yaml
-
-#source activate int_tests
-#behave --no-color --no-logcapture --no-capture
 
 export PATH=/data/users/kkolman/miniconda2/bin:$PATH
 export POLAR2GRID_HOME=/data/dist/polar2grid-swbundle-2.2.1b0
@@ -12,7 +8,7 @@ conda create -y -c conda-forge -n p2g_test python=3 behave
 source activate p2g_test
 cd /data/users/kkolman/integration_tests/polar2grid/integration_tests
 behave --no-logcapture --no-color --no-capture "$WORKSPACE"/integration_tests/features
-exit_status=$?
-source deactivate p2g_test
-exit $exit_status
+#exit_status=$?
+#source deactivate p2g_test
+exit $?
 
