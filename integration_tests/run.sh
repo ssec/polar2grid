43d9608ba915e948e00919a19077e9d7b8a97838
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-06 18:24:44 +0000

    Fixed path

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -5,6 +5,7 @@
 #behave --no-color --no-logcapture --no-capture
 
 export PATH=/data/users/kkolman/miniconda2/bin:$PATH
+export $POLAR2GRID_HOME=/data/dist/polar2grid-swbundle-2.2.1b0
 
 conda remove -y --name p2g_test --all
 conda create -y -c conda-forge -n p2g_test python=3 behave
