cf15b3c4378e1aa3ffa8cde139791af791acc4cb
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-09 16:47:25 +0000

    Added more tests

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -1,15 +1,10 @@
 import sys
     
 def before_all(context):
-    context.numFailed = 0
-    context.data_path = "/data/test_data/old_polar2grid_data/polar2grid_test"
+    context.data_path = "/data/users/kathys/test_data"
     context.p2g_path = "/data/dist/polar2grid-swbundle-2.2.1b0/bin"    
 
-def after_feature(context, feature):
-    if context.failed:
-        context.numFailed += 1
-
 def after_all(context):
-    if context.numFailed > 0:
+    if context.failed:
         sys.exit(1)
 
--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -6,9 +6,8 @@ export POLAR2GRID_HOME=/data/dist/polar2grid-swbundle-2.2.1b0
 conda remove -y --name p2g_test --all
 conda create -y -c conda-forge -n p2g_test python=3 behave
 source activate p2g_test
-cd /data/users/kkolman/integration_tests/polar2grid/integration_tests
-behave --no-logcapture --no-color --no-capture "$WORKSPACE"/integration_tests/features
+cd "$WORKSPACE"/integration_tests
+behave --no-logcapture --no-color --no-capture
 exit_status=$?
 source deactivate p2g_test
 exit $exit_status
-
