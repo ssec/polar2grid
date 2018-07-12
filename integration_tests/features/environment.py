061624ef81293ec2051c37fab7070417694c9736
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-12 19:18:19 +0000

    actually maybe fixed the p2g path

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -4,7 +4,7 @@ def before_all(context):
     context.data_path = "/data/users/kkolman/test_data"
     #context.data_path = "/data/users/kathys/test_data"
     #context.p2g_path = "/data/dist/polar2grid-swbundle-2.2.1b0/bin"    
-    context.p2g_path = "$POLAR2GRID_HOME/test_swbundle/bin"    
+    context.p2g_path = "$POLAR2GRID_HOME/bin"    
 
 def after_all(context):
     if context.failed:
