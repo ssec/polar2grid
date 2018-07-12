f0470770fc7fd694daa3a8e460cdddd0cca07492
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-12 18:30:13 +0000

    changed p2g source dir

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -3,7 +3,8 @@ import sys
 def before_all(context):
     context.data_path = "/data/users/kkolman/test_data"
     #context.data_path = "/data/users/kathys/test_data"
-    context.p2g_path = "/data/dist/polar2grid-swbundle-2.2.1b0/bin"    
+    #context.p2g_path = "/data/dist/polar2grid-swbundle-2.2.1b0/bin"    
+    context.p2g_path = "POLAR2GRID_HOME/test_swbundle"    
 
 def after_all(context):
     if context.failed:
