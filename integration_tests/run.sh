559c573dda4b0b09db4637b4d7161a6eeb4a7dd1
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-12 17:14:50 +0000

    testing software bundling

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -6,6 +6,8 @@ export POLAR2GRID_HOME=/data/dist/polar2grid-swbundle-2.2.1b0
 conda remove -y --name p2g_test --all
 conda create -y -c conda-forge -n p2g_test python=3 behave
 source activate p2g_test
+cd "$WORKSPACE"
+./create_software_bundle test_swbundle /data/dist/ShellB3-Linux-x86_64-CentOS6-20161115-rGIT95e89b0c-portable-core-polar2grid.tar.xz
 cd "$WORKSPACE"/integration_tests
 behave --no-logcapture --no-color --no-capture
 exit_status=$?
