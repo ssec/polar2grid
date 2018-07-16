c193c9507960faa5505ca4bb6d649346cf4da9a6
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-07-16 19:24:48 +0000

    minor changes

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -1,10 +1,10 @@
 import sys
     
 def before_all(context):
-    context.data_path = "/data/users/kkolman/test_data"
     #context.data_path = "/data/users/kathys/test_data"
     #context.p2g_path = "/data/dist/polar2grid-swbundle-2.2.1b0/bin"    
-    context.p2g_path = "$POLAR2GRID_HOME/bin"    
+    context.data_path = "/data/users/kkolman/test_data"
+    context.p2g_path = "$POLAR2GRID_HOME/bin"
 
 def after_all(context):
     if context.failed:
