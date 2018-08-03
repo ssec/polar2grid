38df166470454dae97edeedefc37cf41a81b3768
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-08-03 19:40:40 +0000

    updated paths in behave environment file

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -1,10 +1,9 @@
-import sys
-    
+import sys, os   
+ 
 def before_all(context):
-    #context.data_path = "/data/users/kathys/test_data"
-    #context.p2g_path = "/data/dist/polar2grid-swbundle-2.2.1b0/bin"    
-    context.data_path = "/data/users/kkolman/test_data"
-    context.p2g_path = "$POLAR2GRID_HOME/bin"
+    p2g_home = os.path.expandvars("$POLAR2GRID_HOME")
+    context.data_path = "{}/../integration_tests/p2g_test_data".format(p2g_home)
+    context.p2g_path = "{}/bin".format(p2g_home)
 
 def after_all(context):
     if context.failed:
