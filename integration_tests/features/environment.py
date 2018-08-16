98e3cb078dfa7e526cd1b2fed9688127a514f938
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-08-16 16:25:19 +0000

    fixed minor error

--- integration_tests/features/environment.py
+++ integration_tests/features/environment.py
@@ -4,10 +4,10 @@ def before_all(context):
     p2g_home = os.path.expandvars("$POLAR2GRID_HOME")
     #context.data_path = "{}/../integration_tests/p2g_test_data".format(p2g_home)
     #context.data_path = "/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data"
-    if config.userdata["datapath"].startswith("/"):
-        context.data_path = config.userdata["datapath"]
+    if context.config.userdata["datapath"].startswith("/"):
+        context.data_path = context.config.userdata["datapath"]
     else:
-        context.data_path = os.path.join(os.getcwd(), config.userdata["datapath"])
+        context.data_path = os.path.join(os.getcwd(), context.config.userdata["datapath"])
     print(context.data_path)
     context.p2g_path = "{}/bin".format(p2g_home)
 
