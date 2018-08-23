9575cde88108ed7b243722e2cec57921a85011d8
Katherine Kolman <kkolman@bumi.ssec.wisc.edu>
2018-08-23 19:16:00 +0000

    added behave steps

---
+++ integration_tests/features/steps/data_conversion.py
@@ -0,0 +1,57 @@
+from behave import given, when, then, step
+import os
+import tempfile
+import subprocess
+import shutil
+import glob
+
+
+@given(u'input data from {source}')
+def step_impl(context, source):
+    new_source = ""
+    
+    for f in source.split(" "):
+        f = os.path.join(context.data_path, f) 
+        new_source += f + " "
+
+        if "*" in os.path.basename(f):
+            assert glob.glob(f), "Input files {} do  not exist".format(f)
+            assert os.path.exists(os.path.dirname(f)), "Input folder {} does not exist".format(os.path.dirname(f))
+        else:
+            assert os.path.exists(f), "Input {} does not exist".format(f)
+
+    context.source = new_source
+
+@when(u'{command} runs') 
+def step_impl(context, command):
+    context.command = "{} {} {}".format(os.path.join(context.p2g_path, "polar2grid.sh"), command, context.source)
+
+    # creating new data in temporary directory to compare
+    orig_dir = os.getcwd()
+    try:
+        context.temp_dir = tempfile.mkdtemp(dir="/data/tmp")
+        os.chdir(context.temp_dir)
+        exit_status = subprocess.call(context.command, shell=True)        
+        assert exit_status == 0, "Polar2Grid command ran unsuccessfully"
+    finally:
+        os.chdir(orig_dir)
+    
+    assert os.path.exists(context.temp_dir), "Temporary directory not created"
+    assert os.listdir(context.temp_dir), "No files were created"
+
+@then(u'the output matches with the files in {output}')
+def step_impl(context, output):
+    orig_dir = os.getcwd()
+    try:
+        os.chdir(context.data_path)
+        if "gtiff" in context.command:
+            context.compare_command = "{} {} {}".format(os.path.join(context.data_path, "scripts/p2g_py3_compare_geotiff.sh"), output, context.temp_dir)
+            #context.compare_command = os.path.join(context.data_path, "../polar2grid_test/viirs/p2g_compare_geotiff.sh") + " " + output + " " + context.temp_dir
+        else: 
+            context.compare_command = "{} {} {}".format(os.path.join(context.data_path, "scripts/p2g_py3_compare_netcdf.sh"), output, context.temp_dir)
+            #context.compare_command = os.path.join(context.data_path, "/data/users/kkolman/test_data/scripts/p2g_compare_netcdf.sh") + " " + output + " " + context.temp_dir
+        exit_status = subprocess.call(context.compare_command, shell=True)
+        assert exit_status == 0, "Files did not match with the correct output"
+    finally:
+        os.chdir(orig_dir)
+        shutil.rmtree(context.temp_dir)
