2a64558d8245b95415b342d10dd93b3a92910fb7
David Hoese <david.hoese@ssec.wisc.edu>
2018-11-14 09:16:25 -0600

    Add reader and writer printouts when not supplied to glue script

--- polar2grid/glue.py
+++ polar2grid/glue.py
@@ -106,7 +106,8 @@ def write_scene(scn, writers, writer_args, datasets, to_save=None):
 def add_scene_argument_groups(parser):
     group_1 = parser.add_argument_group(title='Scene Initialization')
     group_1.add_argument('-r', '--reader',
-                         help='Name of reader used to read provided files')
+                         help='Name of reader used to read provided files. '
+                              'Supported readers: ' + ', '.join(['abi_l1b', 'hrit_jma', 'ahi_hsd']))
     group_1.add_argument('-f', '--filenames', nargs='+', default=[],
                          help='Input files to read')
     group_2 = parser.add_argument_group(title='Scene Load')
@@ -161,7 +162,7 @@ def main(argv=sys.argv[1:]):
                         help="When using the 'native' resampler for composites, don't save data "
                              "at its native resolution, use the resolution used to create the "
                              "composite.")
-    parser.add_argument('-w', '--writers', nargs='+', default=['geotiff'],
+    parser.add_argument('-w', '--writers', nargs='+',
                         help='writers to save datasets with')
     parser.add_argument("--list-products", dest="list_products", action="store_true",
                         help="List available reader products and exit")
@@ -173,7 +174,12 @@ def main(argv=sys.argv[1:]):
 
     if args.reader is None:
         parser.print_usage()
-        parser.exit(1, "ERROR: Reader must be provided (-r flag)\n")
+        parser.exit(1, "ERROR: Reader must be provided (-r flag).\n"
+                       "Supported readers:\n\t{}\n".format('\n\t'.join(['abi_l1b', 'ahi_hsd', 'hrit_jma'])))
+    if args.writers is None:
+        parser.print_usage()
+        parser.exit(1, "ERROR: Writer must be provided (-w flag) with one or more writer.\n"
+                       "Supported writers:\n\t{}\n".format('\n\t'.join(['geotiff'])))
 
     glue_name = args.reader + "_" + "-".join(args.writers)
     LOG = logging.getLogger(glue_name)
