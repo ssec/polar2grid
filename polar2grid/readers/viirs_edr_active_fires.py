cac51c09f521e482c3c51ee1036cb95523cf799c
wroberts <wroberts4@wisc.edu>
2019-05-31 16:44:05 -0500

    Add docstrings and correct default resampling

--- polar2grid/readers/viirs_edr_active_fires.py
+++ polar2grid/readers/viirs_edr_active_fires.py
@@ -1,5 +1,34 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# Copyright (C) 2016 Space Science and Engineering Center (SSEC),
+#  University of Wisconsin-Madison.
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file is part of the polar2grid software package. Polar2grid takes
+# satellite observation data, remaps it, and writes it to a file format for
+# input into another program.
+# Documentation: http://www.ssec.wisc.edu/software/polar2grid/
+#
+#     Written by William Roberts and David Hoese    May 2019
+#     University of Wisconsin-Madison
+#     Space Science and Engineering Center
+#     1225 West Dayton Street
+#     Madison, WI  53706
+#     wroberts4@wisc.edu and david.hoese@ssec.wisc.edu
+""""""
 import sys
-
 import logging
 from polar2grid.readers import ReaderWrapper, main
 
@@ -18,7 +47,7 @@ def add_frontend_argument_groups(parser):
     """
     from polar2grid.core.script_utils import ExtendAction
     # Set defaults for other components that may be used in polar2grid processing
-    parser.set_defaults(fornav_D=40, fornav_d=1)
+    parser.set_defaults(remap_method='nearest')
 
     # Use the append_const action to handle adding products to the list
     group_title = "Frontend Initialization"
