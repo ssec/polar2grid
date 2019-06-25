6a38aeda767e4393de9931d1f1b7bd2fa9d99cac
wroberts <wroberts4@wisc.edu>
2019-06-25 13:42:23 -0500

    Try to fix conda not being runnable from shell

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -6,8 +6,8 @@ cd "$WORKSPACE"
 # environment already has polar2grid installed on it
 /data/users/davidh/anaconda3/bin/conda env update -n jenkins_p2g_env -f build_environment.yml
 /data/users/davidh/anaconda3/bin/conda init bash
-. ~/.bashrc
-/data/users/davidh/anaconda3/bin/conda activate jenkins_p2g_env
+source ~/.bashrc
+source activate jenkins_p2g_env
 tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
 ./create_conda_software_bundle.sh ${tarball_name}
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
