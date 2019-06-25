389fadadd132946a8b59a7163f08402442936d47
wroberts <wroberts4@wisc.edu>
2019-06-25 11:45:51 -0500

    try to fix which: no conda in

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -6,10 +6,10 @@ mkdir -p integration_tests/jenkins_p2g_env
 
 # environment already has polar2grid installed on it
 tar -xzf /data/users/kkolman/integration_tests/polar2grid/integration_tests/tarballs/jenkins_p2g_env.tar.gz -C $WORKSPACE/integration_tests/jenkins_p2g_env
-source integration_tests/jenkins_p2g_env/bin/activate
-which conda
+source $WORKSPACE/integration_tests/jenkins_p2g_env/bin/activate
 tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
-./create_conda_software_bundle.sh $tarball_name
+echo $tarball_name
+./create_conda_software_bundle.sh "$tarball_name"
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
