3c51e86445671388a66b7114d4d59a43d3759daa
wroberts <wroberts4@wisc.edu>
2019-06-26 15:22:12 -0500

    Fix tmp directory files not being found

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -13,13 +13,13 @@ echo $new_list
 /data/users/davidh/anaconda3/bin/conda init bash
 source ~/.bashrc
 source activate jenkins_p2g_env
-if [ "$old_list" == "$new_list" ]; then
-    tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
-    ./create_conda_software_bundle.sh "$WORKSPACE/$tarball_name"
-    cp -r "$WORKSPACE/$tarball_name" "/data/users/wroberts/jenkins/."
-else
-    cp -r "/data/users/wroberts/jenkins/$tarball_name" "$WORKSPACE/."
-fi
+#if [ "$old_list" == "$new_list" ]; then
+tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
+./create_conda_software_bundle.sh "$WORKSPACE/$tarball_name"
+cp -r "$WORKSPACE/$tarball_name" "/data/users/wroberts/jenkins/."
+#else
+#    cp -r "/data/users/wroberts/jenkins/$tarball_name" "$WORKSPACE/."
+#fi
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
