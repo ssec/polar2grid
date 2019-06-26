4a509d5a1b531b87bc89d3e88555d233e8de8051
wroberts <wroberts4@wisc.edu>
2019-06-26 08:52:00 -0500

    Debug pyspectral not loading

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -9,8 +9,9 @@ cd "$WORKSPACE"
 source ~/.bashrc
 /data/users/davidh/anaconda3/bin/conda activate jenkins_p2g_env
 tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
-export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 ./create_conda_software_bundle.sh "$WORKSPACE/$tarball_name"
+export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
+/data/users/davidh/anaconda3/bin/conda activate jenkins_p2g_env
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
 exit $?
