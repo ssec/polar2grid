4a9dbc73f9dc543390f1de4139af6be37e37cd6f
wroberts <wroberts4@wisc.edu>
2019-06-26 16:12:29 -0500

    Test saving tarball to load if no differences in environment

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -4,21 +4,21 @@
 cd "$WORKSPACE"
 
 # environment already has polar2grid installed on it
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+ls /var/lib/jenkins/workspace/polar2grid_tests
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 old_list=`/data/users/davidh/anaconda3/bin/conda list -n jenkins_p2g_env`
 /data/users/davidh/anaconda3/bin/conda env update -n jenkins_p2g_env -f build_environment.yml
 /data/users/davidh/anaconda3/bin/conda install -c conda-forge -n jenkins_p2g_env behave
 new_list=`/data/users/davidh/anaconda3/bin/conda list -n jenkins_p2g_env`
-echo $old_list
-echo $new_list
 /data/users/davidh/anaconda3/bin/conda init bash
 source ~/.bashrc
 source activate jenkins_p2g_env
-#if [ "$old_list" == "$new_list" ]; then
+#if [ "$old_list" != "$new_list" ]; then
 tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
 ./create_conda_software_bundle.sh "$WORKSPACE/$tarball_name"
-cp -r "$WORKSPACE/$tarball_name" "/data/users/wroberts/jenkins"
 #else
-#    cp -r "/data/users/wroberts/jenkins/$tarball_name" "$WORKSPACE"
+#    cp -r "/var/lib/jenkins/workspace/polar2grid_tests/polar2grid-swbundle-*" "$WORKSPACE"
 #fi
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
