b0ab656a1eb395012e85b96983760821ed44ad48
wroberts <wroberts4@wisc.edu>
2019-06-27 09:20:18 -0500

    Test saving tarball to load if no differences in environment

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -4,9 +4,6 @@
 cd "$WORKSPACE"
 
 # environment already has polar2grid installed on it
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-ls /var/lib/jenkins/workspace/polar2grid_tests
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 old_list=`/data/users/davidh/anaconda3/bin/conda list -n jenkins_p2g_env`
 /data/users/davidh/anaconda3/bin/conda env update -n jenkins_p2g_env -f build_environment.yml
 /data/users/davidh/anaconda3/bin/conda install -c conda-forge -n jenkins_p2g_env behave
@@ -14,12 +11,29 @@ new_list=`/data/users/davidh/anaconda3/bin/conda list -n jenkins_p2g_env`
 /data/users/davidh/anaconda3/bin/conda init bash
 source ~/.bashrc
 source activate jenkins_p2g_env
-if [ "$old_list" != "$new_list" ]; then
-    tarball_name="polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`"
-    ./create_conda_software_bundle.sh "$WORKSPACE/$tarball_name"
+if [ "$old_list" == "$new_list" ]; then
+    tarball_name=polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`
+    ./create_conda_software_bundle.sh $WORKSPACE/$tarball_name
+    cp -r  $WORKSPACE/$tarball_name /tmp
 else
-    cp -r "/var/lib/jenkins/workspace/polar2grid_tests/polar2grid-swbundle-*" "$WORKSPACE"
+    cp -r /tmp/polar2grid-swbundle-* $WORKSPACE
 fi
+prev_val=0
+prev_fn=""
+for fn in /tmp/polar2grid-swbundle-*
+do
+    var1=`echo ${fn/*polar2grid-swbundle-/} | sed -r 's/[-]+//g'`
+    var1="$((var1))"
+    if [ $var1 -ge $prev_val ]
+    then
+        if [ -s $prev_fn ]
+        then
+            rm -rf $prev_fn
+        fi
+    fi
+    prev_val=$var1
+    prev_fn=$fn
+done
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
