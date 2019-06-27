f3ab1809b9897348cf4daaf331b9c3da30e3834a
wroberts <wroberts4@wisc.edu>
2019-06-27 09:50:16 -0500

    Test saving tarball to load if no differences in environment

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -11,11 +11,26 @@ new_list=`/data/users/davidh/anaconda3/bin/conda list -n jenkins_p2g_env`
 /data/users/davidh/anaconda3/bin/conda init bash
 source ~/.bashrc
 source activate jenkins_p2g_env
-rm -rf tmp/tmpjenkins_*
 if [ "$old_list" != "$new_list" ]; then
     tarball_name=polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`
     ./create_conda_software_bundle.sh $WORKSPACE/$tarball_name
     cp -r  $WORKSPACE/$tarball_name /tmp
+    prev_val=0
+    prev_fn=""
+    for fn in /tmp/polar2grid-swbundle-*
+    do
+        var1=`echo ${fn/*polar2grid-swbundle-/} | sed -r 's/[-]+//g'`
+        var1="$((var1))"
+        if [ $var1 -ge $prev_val ]
+        then
+            if [ -s $prev_fn ]
+            then
+                rm -rf $prev_fn
+            fi
+        fi
+        prev_val=$var1
+        prev_fn=$fn
+    done
 else
     tarball_name=/tmp/polar2grid-swbundle-*
     if [ -s $tarball_name ]
@@ -23,22 +38,7 @@ else
         cp -r $tarball_name $WORKSPACE
     fi
 fi
-prev_val=0
-prev_fn=""
-for fn in /tmp/polar2grid-swbundle-*
-do
-    var1=`echo ${fn/*polar2grid-swbundle-/} | sed -r 's/[-]+//g'`
-    var1="$((var1))"
-    if [ $var1 -ge $prev_val ]
-    then
-        if [ -s $prev_fn ]
-        then
-            rm -rf $prev_fn
-        fi
-    fi
-    prev_val=$var1
-    prev_fn=$fn
-done
+echo $tarball_name
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
