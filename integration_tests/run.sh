bb86e64f8b060a999621c24fef8b5d5d9e0129d6
wroberts <wroberts4@wisc.edu>
2019-07-05 10:28:07 -0500

    Make tarball every time

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -14,33 +14,33 @@ new_list=`/data/users/davidh/anaconda3/bin/conda list -n jenkins_p2g_env`
 source ~/.bashrc
 conda activate jenkins_p2g_env
 tarball_name=`echo /tmp/polar2grid-swbundle-*`
-if [ "$old_list" != "$new_list" ] || [ tarball_name == "/tmp/polar2grid-swbundle-*" ]; then
-    tarball_name=polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`
-    ./create_conda_software_bundle.sh $WORKSPACE/$tarball_name
-    cp -r  $WORKSPACE/$tarball_name /tmp
-    prev_val=0
-    prev_fn=""
-    for fn in /tmp/polar2grid-swbundle-*
-    do
-        var1=`echo ${fn/*polar2grid-swbundle-/} | sed -r 's/[-]+//g'`
-        var1="$((var1))"
-        if [ $var1 -ge $prev_val ]
-        then
-            if [ -s $prev_fn ]
-            then
-                rm -rf $prev_fn
-            fi
-        fi
-        prev_val=$var1
-        prev_fn=$fn
-    done
-else
-    if [ -s $tarball_name ]
+#if [ "$old_list" != "$new_list" ] || [ tarball_name == "/tmp/polar2grid-swbundle-*" ]; then
+tarball_name=polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`
+./create_conda_software_bundle.sh $WORKSPACE/$tarball_name
+cp -r  $WORKSPACE/$tarball_name /tmp
+prev_val=0
+prev_fn=""
+for fn in /tmp/polar2grid-swbundle-*
+do
+    var1=`echo ${fn/*polar2grid-swbundle-/} | sed -r 's/[-]+//g'`
+    var1="$((var1))"
+    if [ $var1 -ge $prev_val ]
     then
-        cp -r $tarball_name $WORKSPACE
+        if [ -s $prev_fn ]
+        then
+            rm -rf $prev_fn
+        fi
     fi
-    tarball_name=${tarball_name/\/tmp\//}
-fi
+    prev_val=$var1
+    prev_fn=$fn
+done
+#else
+#    if [ -s $tarball_name ]
+#    then
+#        cp -r $tarball_name $WORKSPACE
+#    fi
+#    tarball_name=${tarball_name/\/tmp\//}
+#fi
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
