82b0898d226ff7af5976a02de408dd85df52b678
wroberts <wroberts4@wisc.edu>
2019-07-16 11:42:19 -0500

    Update tarball directory when saving

--- integration_tests/save_bundle.sh
+++ integration_tests/save_bundle.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 # Only run by Jenkins if build was successful.
 
-cp -r $WORKSPACE/$tarball_name /tmp
-prev_val=0
-prev_fn=""
 for fn in /tmp/polar2grid-swbundle-*
 do
-    new_val=`echo ${fn/*polar2grid-swbundle-/} | sed -r 's/[-]+//g'`
-    new_val="$((new_val))"
-    if [ new_val -ge $prev_val ]
-    then
-        if [ -s $prev_fn ]
-        then
-            rm -rf $prev_fn
-        fi
-    fi
-    prev_val=new_val
-    prev_fn=$fn
+    rm -rf $fn
 done
+cp -r $POLAR2GRID_HOME /tmp
 exit $?
