06430597927139d987f84e32a889f42967c94e49
wroberts <wroberts4@wisc.edu>
2019-07-16 11:55:46 -0500

    Move post build scripts to build script

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -15,5 +15,21 @@ tarball_name=polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
 behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/integration_tests/polar2grid/integration_tests/p2g_test_data
-echo $?
+# Only run by Jenkins if build was successful.
+if [ $? = 0 ]
+then
+    # Save software bundle.
+    for fn in /tmp/polar2grid-swbundle-*
+    do
+        rm -rf $fn
+    done
+    cp -r $POLAR2GRID_HOME /tmp
+    # Make docs.
+    conda activate jenkins_p2g_env
+    conda install -y sphinx
+    pip install sphinx-argparse
+    cd "$WORKSPACE"/doc
+    make html
+    cp -r "$WORKSPACE"/doc/build/html /data/users/wroberts/html/.
+fi
 exit $?
--- integration_tests/save_bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Only run by Jenkins if build was successful.
-
-for fn in /tmp/polar2grid-swbundle-*
-do
-    rm -rf $fn
-done
-cp -r $POLAR2GRID_HOME /tmp
-exit $?
