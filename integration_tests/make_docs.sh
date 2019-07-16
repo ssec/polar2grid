35120d2f842d2874094b729c0a7fb2b04958a503
wroberts <wroberts4@wisc.edu>
2019-07-16 11:03:16 -0500

    Try to fix tarball not being made

--- integration_tests/make_docs.sh
+++ integration_tests/make_docs.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+# Activate conda for bash.
+/data/users/davidh/anaconda3/bin/conda init bash
+# Restart the shell to enable conda.
+source ~/.bashrc
+conda activate jenkins_p2g_env
+
+
+
 exit $?
