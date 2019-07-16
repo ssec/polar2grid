bc9eb9268f66e93a46e8c063a3fec434bbb81a08
wroberts <wroberts4@wisc.edu>
2019-07-16 12:29:04 -0500

    Make html and bundle in one directory under /tmp

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script for jenkins to run tests on polar2grid.
 
+rm -rf /data/users/wroberts/docs
 cd "$WORKSPACE"
 # Activate conda for bash.
 /data/users/davidh/anaconda3/bin/conda init bash
@@ -10,7 +11,8 @@ conda remove -y -n jenkins_p2g_env --all
 conda env update -n jenkins_p2g_env -f build_environment.yml
 conda activate jenkins_p2g_env
 
-tarball_name=polar2grid-swbundle-`date +"%Y%m%d-%H%M%S"`
+time=`date +"%Y%m%d-%H%M%S"`
+tarball_name=polar2grid-swbundle-$time
 ./create_conda_software_bundle.sh $WORKSPACE/$tarball_name
 export POLAR2GRID_HOME="$WORKSPACE/$tarball_name"
 cd "$WORKSPACE/integration_tests"
@@ -18,17 +20,18 @@ behave --no-logcapture --no-color --no-capture -D datapath=/data/users/kkolman/i
 # Only run by Jenkins if build was successful.
 if [ $? = 0 ]
 then
+    mkdir /tmp/polar2grid-$time
     # Save software bundle.
-    for fn in /tmp/polar2grid-swbundle-*
+    for fn in /tmp/polar2grid-*
     do
         rm -rf $fn
     done
-    cp -r $POLAR2GRID_HOME /tmp
+    cp -r $POLAR2GRID_HOME /tmp/polar2grid-$time
     # Make docs.
     conda install -y sphinx
     pip install sphinx-argparse
     cd "$WORKSPACE"/doc
     make html
-    cp -r "$WORKSPACE"/doc/build/html /data/users/wroberts/docs
+    cp -r "$WORKSPACE"/doc/build/html /tmp/polar2grid-$time
 fi
 exit $?
