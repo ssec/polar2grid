844d9c23f964578787fb0edc62e2f1cfbd842b74
wroberts <wroberts4@wisc.edu>
2019-09-11 13:38:39 -0500

    Clean up run.py

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -10,21 +10,13 @@ cd "$WORKSPACE"
 # Restart the shell to enable conda.
 source ~/.bashrc
 
-# Documentation environment also has behave, while the build environment does not.
-conda env update -n jenkins_p2g_docs -f "$WORKSPACE/build_environment.yml"
-conda env update -n jenkins_p2g_docs -f "$WORKSPACE/jenkins_environment.yml"
-conda activate jenkins_p2g_docs
-pip install "$WORKSPACE"
-
 commit_message=`git log --format=%B -n 1 $GIT_COMMIT`
-
 if [[ "$(cut -d'-' -f1 <<<"$GIT_TAG_NAME")" = "g2g" ]] || [[ "$commit_message" =~ (^|.[[:space:]])"["g2g-skip-tests"]"$ ]]
 then
     prefix=geo
 else
     prefix=polar
 fi
-
 # Handle release vs test naming.
 end="`date +"%Y%m%d-%H%M%S"`"
 # If the tag is correct and a version was specified, make a version release.
@@ -32,8 +24,13 @@ if [[ "$GIT_TAG_NAME" =~ ^[pg]2g-v[0-9]+\.[0-9]+\.[0-9]+.* ]]; then
     # Removes prefix from $GIT_TAG_NAME.
     end="${GIT_TAG_NAME:5}"
 fi
-
 swbundle_name="${prefix}2grid-swbundle-${end}"
+
+# Documentation environment also has behave, while the build environment does not.
+conda env update -n jenkins_p2g_docs -f "$WORKSPACE/build_environment.yml"
+conda env update -n jenkins_p2g_docs -f "$WORKSPACE/jenkins_environment.yml"
+conda activate jenkins_p2g_docs
+pip install "$WORKSPACE"
 conda env update -n jenkins_p2g_swbundle -f "$WORKSPACE/build_environment.yml"
 conda activate jenkins_p2g_swbundle
 ./create_conda_software_bundle.sh "${WORKSPACE}/${swbundle_name}"
@@ -57,7 +54,6 @@ make html POLAR2GRID_DOC="${prefix}"
 cp -r "$WORKSPACE"/doc/build/html "/tmp/${prefix}2grid-${end}"
 chmod -R a+rX "/tmp/${prefix}2grid-${end}"
 # Only copy to data/dist if the tag was correct, a version was specified, and the docs were successfully made.
-if [[ "$GIT_TAG_NAME" =~ ^[pg]2g-v[0-9]+\.[0-9]+\.[0-9]+.* ]] && [[ ! "$commit_message" =~ (^|.[[:space:]])"["[pg]2g-skip-tests"]"$ ]]
-then
+if [[ "$GIT_TAG_NAME" =~ ^[pg]2g-v[0-9]+\.[0-9]+\.[0-9]+.* ]]; then
     cp "${WORKSPACE}/${swbundle_name}.tar.gz" /data/dist
 fi
