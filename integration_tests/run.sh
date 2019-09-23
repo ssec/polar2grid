aaca53a2ff4d711e8367ae9945fee6962d5d68f1
wroberts <wroberts4@wisc.edu>
2019-09-23 09:35:49 -0500

    Try to fix packages not found

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -27,11 +27,11 @@ fi
 swbundle_name="${prefix}2grid-swbundle-${end}"
 
 # Documentation environment also has behave, while the build environment does not.
-conda env update -n jenkins_p2g_docs --file "$WORKSPACE/build_environment.yml"  --update-all
-conda env update -n jenkins_p2g_docs --file "$WORKSPACE/jenkins_environment.yml" --update-all
+conda create -n jenkins_p2g_docs --file "$WORKSPACE/build_environment.yml" --force
+conda env update -n jenkins_p2g_docs --file "$WORKSPACE/jenkins_environment.yml"
 conda activate jenkins_p2g_docs
 pip install "$WORKSPACE"
-conda env update -n jenkins_p2g_swbundle --file "$WORKSPACE/build_environment.yml" --update-all
+conda create -n jenkins_p2g_swbundle --file "$WORKSPACE/build_environment.yml" --force
 conda activate jenkins_p2g_swbundle
 ./create_conda_software_bundle.sh "${WORKSPACE}/${swbundle_name}"
 conda activate jenkins_p2g_docs
