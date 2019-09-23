27ac3da72abf70d5153c48b6992b72f95156d9b2
wroberts <wroberts4@wisc.edu>
2019-09-23 15:51:17 -0500

    Try to fix xarray not found

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -30,13 +30,13 @@ swbundle_name="${prefix}2grid-swbundle-${end}"
 conda env update -n jenkins_p2g_docs --file "$WORKSPACE/build_environment.yml"
 conda activate jenkins_p2g_docs
 pip install -U git+https://github.com/pytroll/satpy
-conda env update -n jenkins_p2g_docs --file "$WORKSPACE/build_environment.yml"
+conda install xarray
 conda env update -n jenkins_p2g_docs --file "$WORKSPACE/jenkins_environment.yml"
 pip install -U --no-deps "$WORKSPACE"
 conda env update -n jenkins_p2g_swbundle --file "$WORKSPACE/build_environment.yml"
 conda activate jenkins_p2g_swbundle
 pip install -U git+https://github.com/pytroll/satpy
-conda env update -n jenkins_p2g_swbundle --file "$WORKSPACE/build_environment.yml"
+conda install xarray
 ./create_conda_software_bundle.sh "${WORKSPACE}/${swbundle_name}"
 conda activate jenkins_p2g_docs
 if [[ ! "$commit_message" =~ (^|.[[:space:]])"["[pg]2g-skip-tests"]"$ ]]; then
