68becad1a3b13c15d249936e55a5cf81187f3c48
David Hoese <david.hoese@ssec.wisc.edu>
2021-03-22 08:55:42 -0500

    Add Satpy auxiliary data download to bundle creation

--- create_conda_software_bundle.sh
+++ create_conda_software_bundle.sh
@@ -132,6 +132,15 @@ $SB_NAME/bin/download_pyspectral_data.sh || oops "Couldn't download pyspectral d
 # Add the download_from_internet: False to the config
 echo "download_from_internet: False" >> ${SB_NAME}/etc/pyspectral.yaml
 
+# Download Satpy auxiliary data
+echo "Downloading Satpy auxiliary data..."
+AUX_CACHE_DIR="${CACHE_DIR}/satpy_aux_data"
+SATPY_DATA_DIR="${SB_NAME}/share/polar2grid/data"
+${SB_NAME}/bin/satpy_retrieve_all_aux_data --data-dir ${AUX_CACHE_DIR}
+echo "Copying Satpy auxiliary data to software bundle..."
+# don't include large geotiff files that we don't use in P2G/G2G
+rsync -auv --exclude "*.tif" ${AUX_CACHE_DIR}/* "${SATPY_DATA_DIR}/"
+
 # Perform extra "risky" operations to make the tarball as small as possible
 # Taken from https://jcrist.github.io/conda-docker-tips.html
 MINIFY_TARBALL=${MINIFY_TARBALL:-1}
