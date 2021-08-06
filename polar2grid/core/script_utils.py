258858222b6db2ec6bbc4e85f4edb59df66c99a3
David Hoese <david.hoese@ssec.wisc.edu>
2021-08-05 19:34:42 -0500

    Switch geotiff writer to tile by default (#365)
    Allows for --tiled (default) or --no-tiled

--- polar2grid/core/script_utils.py
+++ polar2grid/core/script_utils.py
@@ -47,6 +47,11 @@ import argparse
 from collections import defaultdict
 from glob import glob
 
+try:
+    from argparse import BooleanOptionalAction
+except ImportError:
+    BooleanOptionalAction = None
+
 LOG = logging.getLogger(__name__)
 
 
@@ -110,7 +115,7 @@ def setup_logging(console_level=logging.INFO, log_filename="polar2grid.log", log
     # Log file messages have a lot more information
     if log_filename:
         file_handler = logging.FileHandler(log_filename)
-        file_format = "[%(asctime)s] : PID %(process)6d : %(levelname)-8s : %(name)s : %(funcName)s : %(message)s"
+        file_format = "[%(asctime)s] : %(levelname)-8s : %(name)s : %(funcName)s : %(message)s"
         file_handler.setFormatter(logging.Formatter(file_format))
         file_handler.setLevel(logging.DEBUG)
         root_logger.addHandler(file_handler)
@@ -353,3 +358,53 @@ def create_basic_parser(*args, **kwargs):
         help="exit on first error including non-fatal errors",
     )
     return parser
+
+
+# Backport of BooleanOptionalAction (added in Python 3.9)
+class _BooleanOptionalAction(argparse.Action):
+    def __init__(
+        self,
+        option_strings,
+        dest,
+        const=None,
+        default=None,
+        type=None,
+        choices=None,
+        required=False,
+        help=None,
+        metavar=None,
+    ):
+
+        _option_strings = []
+        for option_string in option_strings:
+            _option_strings.append(option_string)
+
+            if option_string.startswith("--"):
+                option_string = "--no-" + option_string[2:]
+                _option_strings.append(option_string)
+
+        if help is not None and default is not None:
+            help += f" (default: {default})"
+
+        super().__init__(
+            option_strings=_option_strings,
+            dest=dest,
+            nargs=0,
+            default=default,
+            type=type,
+            choices=choices,
+            required=required,
+            help=help,
+            metavar=metavar,
+        )
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if option_string in self.option_strings:
+            setattr(namespace, self.dest, not option_string.startswith("--no-"))
+
+    def format_usage(self):
+        return " | ".join(self.option_strings)
+
+
+if BooleanOptionalAction is None:
+    BooleanOptionalAction = _BooleanOptionalAction
