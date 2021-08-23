03a1d9fc8142bcacad5e099e9668e6a96a9a10db
David Hoese <david.hoese@ssec.wisc.edu>
2021-08-23 14:26:42 -0500

    Port VIIRS EDR Active Fires reader for Polar2Grid 3.0

--- polar2grid/readers/viirs_edr_active_fires.py
+++ polar2grid/readers/viirs_edr_active_fires.py
@@ -20,13 +20,6 @@
 # satellite observation data, remaps it, and writes it to a file format for
 # input into another program.
 # Documentation: http://www.ssec.wisc.edu/software/polar2grid/
-#
-#     Written by William Roberts and David Hoese    May 2019
-#     University of Wisconsin-Madison
-#     Space Science and Engineering Center
-#     1225 West Dayton Street
-#     Madison, WI  53706
-#     wroberts4@wisc.edu and david.hoese@ssec.wisc.edu
 """
 The VIIRS EDR Active Fires reader operates on CSPP NetCDF I-Band (AFIMG) Resolution or 
 M-Band Resolution (AFMOD) Environmental Data Record files.
@@ -52,49 +45,46 @@ The ``--remap_method`` parameter is set to ``nearest``.
 | T13                       | M-Band 13 Temperature      (AFMOD Resolution Only)  |
 +---------------------------+-----------------------------------------------------+
 """
-import sys
-import logging
-from polar2grid.readers import ReaderWrapper, main
+from __future__ import annotations
 
-LOG = logging.getLogger(__name__)
+from argparse import ArgumentParser, _ArgumentGroup
+from typing import Optional
+
+from ._base import ReaderProxyBase
+
+from satpy import DataQuery
 
 
-class Frontend(ReaderWrapper):
-    FILE_EXTENSIONS = ['.nc', '.txt']
-    DEFAULT_READER_NAME = 'viirs_edr_active_fires'
-    # confidence_cat and T4 are IMG resolution only
-    # confidence_pct and T13 are MOD resolution only
-    DEFAULT_DATASETS = ['T4', 'T13', 'confidence_cat', 'confidence_pct', 'power']
+DEFAULT_DATASETS = ["T4", "T13", "confidence_cat", "confidence_pct", "power"]
+
+
+class ReaderProxy(ReaderProxyBase):
+    """Provide Polar2Grid-specific information about this reader's products."""
+
+    is_polar2grid_reader = True
+
+    def get_default_products(self) -> list[str]:
+        """Get products to load if users hasn't specified any others."""
+        return DEFAULT_DATASETS
+
+    def get_all_products(self) -> list[str]:
+        """Get all polar2grid products that could be loaded."""
+        return DEFAULT_DATASETS
 
     @property
-    def available_product_names(self):
-        available = set(self.scene.available_dataset_names(reader_name=self.reader, composites=True))
-        # remove longitude and latitude as available datasets to avoid confusion
-        available -= {'longitude', 'latitude'}
-        return sorted(available & set(self.all_product_names))
+    def _aliases(self) -> dict[DataQuery]:
+        return {}
 
 
-def add_frontend_argument_groups(parser):
-    """Add command line arguments to an existing parser.
+def add_reader_argument_groups(
+    parser: ArgumentParser, group: Optional[_ArgumentGroup] = None
+) -> tuple[Optional[_ArgumentGroup], Optional[_ArgumentGroup]]:
+    """Add reader-specific command line arguments to an existing argument parser.
 
-    :returns: list of group titles added
+    If ``group`` is provided then arguments are added to this group. If not,
+    a new group is added to the parser and arguments added to this new group.
+
     """
-    from polar2grid.core.script_utils import ExtendAction
-    # Set defaults for other components that may be used in polar2grid processing
-    parser.set_defaults(remap_method='nearest', grid_coverage=0)
-
-    # Use the append_const action to handle adding products to the list
-    group_title = "Frontend Initialization"
-    group = parser.add_argument_group(title=group_title, description="swath extraction initialization options")
-    group.add_argument("--list-products", dest="list_products", action="store_true",
-                       help="List available frontend products and exit")
-    group_title = "Frontend Swath Extraction"
-    group = parser.add_argument_group(title=group_title, description="swath extraction options")
-    group.add_argument("-p", "--products", dest="products", nargs="+", default=None, action=ExtendAction,
-                       help="Specify frontend products to process")
-    return ["Frontend Initialization", "Frontend Swath Extraction"]
-
-
-if __name__ == "__main__":
-    sys.exit(main(description="Extract VIIRS_EDR_ACTIVE_FIRES swath data into binary files",
-                  add_argument_groups=add_frontend_argument_groups))
+    if group is None:
+        group = parser.add_argument_group(title="VIIRs EDr Active Fires Reader")
+    return group, None
