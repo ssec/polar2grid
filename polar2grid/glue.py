ba2ab5537e913df0608ee1096275821172256402
David Hoese <david.hoese@ssec.wisc.edu>
2022-04-08 09:57:21 -0500

    Fix AVHRR reader using "avhrr-3" name instead of "avhrr"

--- polar2grid/glue.py
+++ polar2grid/glue.py
@@ -82,6 +82,11 @@ _PLATFORM_ALIASES = {
 }
 
 
+_SENSOR_ALIASES = {
+    "avhrr-3": "avhrr",
+}
+
+
 def _get_platform_name_alias(satpy_platform_name):
     return _PLATFORM_ALIASES.get(satpy_platform_name.lower(), satpy_platform_name)
 
@@ -93,6 +98,24 @@ def _overwrite_platform_name_with_aliases(scn):
             continue
         pname = _get_platform_name_alias(data_arr.attrs["platform_name"])
         data_arr.attrs["platform_name"] = pname
+
+
+def _get_sensor_alias(satpy_sensor):
+    if not isinstance(satpy_sensor, set):
+        satpy_sensor = {satpy_sensor}
+    new_sensor = {_SENSOR_ALIASES.get(sname, sname) for sname in satpy_sensor}
+    if len(new_sensor) == 1:
+        return new_sensor.pop()
+    return new_sensor
+
+
+def _overwrite_sensor_with_aliases(scn):
+    """Change 'sensor' for every DataArray to Polar2Grid expectations."""
+    for data_arr in scn:
+        if "sensor" not in data_arr.attrs:
+            continue
+        pname = _get_sensor_alias(data_arr.attrs["sensor"])
+        data_arr.attrs["sensor"] = pname
 
 
 def _write_scene(
@@ -230,6 +253,7 @@ def _save_scenes(scenes_to_save: list[tuple], reader_info, writer_args) -> list:
     to_save = []
     for scene_to_save, products_to_save in scenes_to_save:
         _overwrite_platform_name_with_aliases(scene_to_save)
+        _overwrite_sensor_with_aliases(scene_to_save)
         reader_info.apply_p2g_name_to_scene(scene_to_save)
         to_save = _write_scene(
             scene_to_save,
