8f8f693e4f01957d6b6d16dcdf2364de57b350fa
David Hoese <david.hoese@ssec.wisc.edu>
2025-04-30 10:57:06 -0500

    Remove unused legacy GridDefinition methods

--- polar2grid/core/containers.py
+++ polar2grid/core/containers.py
@@ -22,13 +22,6 @@
 # Documentation: http://www.ssec.wisc.edu/software/polar2grid/
 """Classes for metadata operations in polar2grid."""
 
-import logging
-
-import numpy
-from pyproj import Proj
-
-LOG = logging.getLogger(__name__)
-
 
 class GridDefinition(dict):
     """Projected grid defined by a PROJ.4 projection string and other grid parameters.
@@ -56,12 +49,10 @@ class GridDefinition(dict):
     )
 
     def __init__(self, *args, **kwargs):
-        # pyproj.Proj object if needed
-        self.p = None
         if "proj4_definition" in kwargs:
             # pyproj doesn't like unicode
             kwargs["proj4_definition"] = str(kwargs["proj4_definition"])
-        super(GridDefinition, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.validate_keys()
 
     def validate_keys(self):
@@ -81,53 +72,10 @@ class GridDefinition(dict):
         return "\n".join("%s: %s" % (k, self[k]) for k in keys)
 
     @property
-    def proj(self):
-        if self.p is None:
-            self.p = Proj(self["proj4_definition"])
-        return self.p
-
-    @property
     def is_static(self):
         return all(
             [self[x] is not None for x in ["height", "width", "cell_height", "cell_width", "origin_x", "origin_y"]]
         )
-
-    @property
-    def lonlat_lowerleft(self):
-        x_ll, y_ll = self.xy_lowerleft
-        return self.proj(x_ll, y_ll, inverse=True)
-
-    @property
-    def lonlat_lowerright(self):
-        x_ll, y_ll = self.xy_lowerright
-        return self.proj(x_ll, y_ll, inverse=True)
-
-    @property
-    def lonlat_upperright(self):
-        x_ur, y_ur = self.xy_upperright
-        return self.proj(x_ur, y_ur, inverse=True)
-
-    @property
-    def lonlat_upperleft(self):
-        x_ur, y_ur = self["origin_x"], self["origin_y"]
-        return self.proj(x_ur, y_ur, inverse=True)
-
-    @property
-    def lonlat_center(self):
-        x_pixel = self["width"] / 2
-        y_pixel = self["height"] / 2
-        return self.get_lonlat(x_pixel, y_pixel)
-
-    def get_lonlat(self, x_pixel, y_pixel):
-        x = self["origin_x"] + x_pixel * self["cell_width"]
-        y = self["origin_y"] + y_pixel * self["cell_height"]
-        return self.proj(x, y, inverse=True)
-
-    @property
-    def xy_lowerright(self):
-        y_ll = self["origin_y"] + self["cell_height"] * (self["height"] - 1)
-        x_ll = self["origin_x"] + self["cell_width"] * (self["width"] - 1)
-        return x_ll, y_ll
 
     @property
     def xy_lowerleft(self):
@@ -197,83 +145,16 @@ class GridDefinition(dict):
         return proj4_dict
 
     @property
-    def gdal_geotransform(self):
-        # GDAL PixelIsArea (default) geotiffs expects coordinates for the
-        # upper-left corner of the pixel
-        # Polar2Grid and satellite imagery formats assume coordinates for
-        # center of the pixel
-        half_pixel_x = self["cell_width"] / 2.0
-        half_pixel_y = self["cell_height"] / 2.0
-        # cell_height is negative so we need to substract instead of add
-        return (
-            self["origin_x"] - half_pixel_x,
-            self["cell_width"],
-            0,
-            self["origin_y"] - half_pixel_y,
-            0,
-            self["cell_height"],
-        )
-
-    def get_xy_arrays(self):
-        # the [None] indexing adds a dimension to the array
-        x = self["origin_x"] + numpy.repeat(
-            numpy.arange(self["width"])[None] * self["cell_width"], self["height"], axis=0
-        )
-        y = self["origin_y"] + numpy.repeat(
-            numpy.arange(self["height"])[:, None] * self["cell_height"], self["width"], axis=1
-        )
-        return x, y
-
-    def get_geolocation_arrays(self):
-        """Calculate longitude and latitude arrays for the grid.
-
-        :returns: (longitude array, latitude array)
-        """
-        x, y = self.get_xy_arrays()
-        return self.proj(x, y, inverse=True)
-
-    def to_basemap_object(self):
-        from mpl_toolkits.basemap import Basemap
-
-        proj4_dict = self.proj4_dict
-        proj4_dict.pop("no_defs", None)
-        proj4_dict.pop("units", None)
-        proj4_dict["projection"] = proj4_dict.pop("proj")
-
-        if "a" in proj4_dict and "b" in proj4_dict:
-            a = proj4_dict.pop("a")
-            b = proj4_dict.pop("b")
-            proj4_dict["rsphere"] = (a, b)
-        elif "a" in proj4_dict:
-            proj4_dict["rsphere"] = proj4_dict.pop("a")
-        elif "b" in proj4_dict:
-            proj4_dict["rsphere"] = proj4_dict.pop("b")
-
-        lon_ll, lat_ll = self.lonlat_lowerleft
-        lon_ur, lat_ur = self.lonlat_upperright
-        LOG.debug("Passing basemap the following keywords from PROJ.4: %r", proj4_dict)
-        LOG.debug("Lower-left corner: (%f, %f); Upper-right corner: (%f, %f)", lon_ll, lat_ll, lon_ur, lat_ur)
-        return Basemap(llcrnrlon=lon_ll, llcrnrlat=lat_ll, urcrnrlon=lon_ur, urcrnrlat=lat_ur, **proj4_dict)
-
-    @property
     def ll_extent(self):
         xy_ll = self.xy_lowerleft
         # NOTE: cell_height is negative
         return xy_ll[0] - self["cell_width"] / 2.0, xy_ll[1] + self["cell_height"] / 2.0
 
     @property
-    def ll_extent_lonlat(self):
-        return self.proj(*self.ll_extent, inverse=True)
-
-    @property
     def ur_extent(self):
         xy_ur = self.xy_upperright
         # NOTE: cell_height is negative
         return xy_ur[0] + self["cell_width"] / 2.0, xy_ur[1] - self["cell_height"] / 2.0
-
-    @property
-    def ur_extent_lonlat(self):
-        return self.proj(*self.ur_extent, inverse=True)
 
     def to_satpy_area(self):
         from pyresample.geometry import AreaDefinition, DynamicAreaDefinition
