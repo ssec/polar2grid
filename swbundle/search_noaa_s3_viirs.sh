da1c2569c367ed3460482ada93298e2997627843
David Hoese <david.hoese@ssec.wisc.edu>
2026-04-24 15:13:09 -0500

    Add bash wrapper script for S3 search script

---
+++ swbundle/search_noaa_s3_viirs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+### Verify simple test products with known products ###
+#
+# Copyright (C) 2026 Space Science and Engineering Center (SSEC),
+#  University of Wisconsin-Madison.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This file is part of the polar2grid software package. Polar2grid takes
+# satellite observation data, remaps it, and writes it to a file format for
+# input into another program.
+# Documentation: http://www.ssec.wisc.edu/software/polar2grid/
+
+export POLAR2GRID_HOME="$( cd -P "$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && cd .. && pwd )"
+
+# Setup necessary environments
+# __SWBUNDLE_ENVIRONMENT_INJECTION__
+
+# Run tests for each test data directory in the base directory
+python3 -m polar2grid.utils.search_noaa_s3_viirs "$@"
